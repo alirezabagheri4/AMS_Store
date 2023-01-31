1334fdf5ff017888f88b5188ede5ed8d649c0a45
Alireza Bagheri <78638285+alirezabagheri4@users.noreply.github.com>
2023-01-31 09:57:31 +0330

    ...

---
+++ Modules/PMS/4-Infra.Data/Infra.Data/Infra.Data/Persistence/Repository/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Persistence.Repository
+{
+    internal class ProductRepository
+    {
+    }
+}
